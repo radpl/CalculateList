c4d512e20b768fe014112268961551577efeb872
Radoslaw Plonka <radoslaw.a.plonka@gmail.com>
2019-11-15 01:02:48 +0100

    Work in progress

---
+++ CalculateList/IntegerList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateList
+{
+    public class IntegerList
+    {
+
+        private readonly int[] _list;
+        private int min, max, sum;
+
+        public IntegerList(int[] list)
+        {
+            _list = list;
+            min = max = _list[0];
+            Calculate();
+        }
+
+        private void Calculate()
+        {
+            //Calculate Min, Max and Sum
+            foreach (var el in _list)
+            {
+                if (el < min) min = el;
+                if (el > max) max = el;
+                sum += el;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Join(",", _list) + " " + min + " " + max + " " + sum ;
+        }
+    }
+}
--- CalculateList/Program.cs
+++ CalculateList/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace CalculateList
 {
@@ -10,6 +7,59 @@ namespace CalculateList
     {
         static void Main(string[] args)
         {
+            if (Validate(args))
+            {
+                int[] arr = Process(args);
+                var list = new IntegerList(arr);
+                Console.WriteLine(list);
+            }
+
+            //Console.WriteLine($"My input: {args[0]}");
+            // Here some magic...
+            Console.WriteLine("Max number is: ??");
+            Console.WriteLine("Min number is: ??");
+            Console.WriteLine("Average is: ??");
+            Console.WriteLine("Std Deviation is: ??");
+            Console.ReadLine();
         }
+       
+        public static int[] Process(string[] args)
+        {
+                string list = args[0];
+                string[] stringArr = list.Split(',');
+                int[] values = Array.ConvertAll(stringArr, int.Parse);
+                return values;
+        }
+
+        public static bool Validate(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                Console.WriteLine("No parameter. One parameter is required. Example: 1,5,4,12,21");
+                return false;
+            }
+            else if (args.Length > 1)
+            {
+                Console.WriteLine($"To many parameters. One is required. Example: 1,5,4,12,21");
+                return false;
+            }
+  
+            string commaSeparatedList = @"^[0-9]+(,[0-9]+)*$";
+            string input = args[0];
+            Match m = Regex.Match(input, commaSeparatedList, RegexOptions.IgnoreCase);
+            if (m.Success)
+            {
+                Console.WriteLine("Correct list of values found '{0}' ", m.Value);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong");
+            }
+
+            return false;
+        }
+
+
     }
 }
